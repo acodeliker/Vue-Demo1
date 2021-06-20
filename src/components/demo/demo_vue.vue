3790dbd8b622e210cdcff910db4ea45568a06db3
acodeliker <1263956078@qq.com>
2021-06-20 21:13:10 +0800

    add simple vue proj

---
+++ src/components/demo/demo_vue.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div id="vue"> Hello Vue.js! {{ message }}</div>
+</template>
+
+<script type="test/javascript">
+export default {
+  name: "HelloVue",
+  data() {
+    return {
+      message: "优美的中国话"
+    }
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style type="text/css">
+  #vue{
+    color: green;
+    font-size: 28px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
+
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import HelloVue from '@/components/demo/demo_vue'
+
 
 Vue.use(Router)
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: "/demo",
+      name: 'HelloVue',
+      component: HelloVue
     }
   ]
 })
